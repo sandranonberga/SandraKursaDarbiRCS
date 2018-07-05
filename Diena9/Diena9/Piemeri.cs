ac6a9849a63b1b24839a5cc8dc6e5d1b979f50cc
Sandra Nonberga <sandra.nonberga@gmail.com>
2018-07-05 19:32:16 +0300

    Diena 9 added

---
+++ Diena9/Diena9/Piemeri.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diena9
+{
+	class Piemeri
+	{
+		string[] masivs = new string[12];
+
+		public void Klase()
+		{
+			//12 vietas, Ar ConsoleReadLine - vai vieta ir brīva vai tukša. Tukšā - 0, pilnā - 1; 
+
+			
+			Console.WriteLine("Ievadiet, vai solaa kaads seezh:");
+			string vards = "";
+
+			for (int i = 0; i < 12; i++)
+			{
+				vards = Console.ReadLine();
+				masivs[i] = vards;
+			}
+
+			Console.WriteLine("");
+
+			for (int i = 0; i < 12; i++)
+			{
+				if (masivs[i] != "0")
+				{
+					Console.WriteLine("Pie galda sez " + masivs[i]);
+				}
+			}
+
+
+
+
+		}
+
+	}
+}
---
+++ Diena9/Diena9/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diena9
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Piemeri obj = new Piemeri();
+			obj.Klase();
+
+			Console.ReadLine();
+
+		}
+	}
+}
