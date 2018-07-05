b41386cd3690a2538fc6498619a136b82b3adc93
Sandra Nonberga <sandra.nonberga@gmail.com>
2018-07-05 21:24:00 +0300

    Praktiskais-2 added

---
+++ Praktiskais-2/Praktiskais-2/Kalkulators.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktiskais_2
+{
+	class Kalkulators
+	{
+		
+		public void funkcizv()
+		{
+			Console.WriteLine("Ievadiet pirmo ciparu:");
+			string pirmaissk = Console.ReadLine();
+			double a = Convert.ToDouble(pirmaissk);
+			Console.WriteLine("Ievadiet otro ciparu:");
+			string otraissk = Console.ReadLine();
+			double b = Convert.ToDouble(otraissk);
+
+			Console.WriteLine("Izveelieties uzdevumu:");
+			Console.WriteLine("1 - saskaitishana, 2 - atnemshana, 3 - reizinashana, 4 - dalishana");
+			string input = Console.ReadLine();
+
+			Console.WriteLine("Rezultats ir:");
+
+			switch (input)
+			{
+				case "1":
+					Saskaitit(a,b);
+					break;
+				case "2":
+					Atnemt(a, b);
+					break;
+				case "3":
+					Reizinat(a, b);
+					break;
+				case "4":
+					Dalit(a, b);
+					break;
+				case "5":
+					Kapinat(a, b);
+					break;
+				default:
+					Console.WriteLine("Nepareiza ievade");
+					break;
+
+			}
+
+			Console.WriteLine();
+		}
+
+
+		private double Saskaitit(double a, double b)
+		{
+			double rezultats = a + b;
+
+			return rezultats;
+		}
+
+
+		private double Atnemt(double a, double b)
+		{
+			double rezultats = a - b;
+
+			return rezultats;
+		}
+
+
+		private double Reizinat(double a, double b)
+		{
+			double rezultats = a * b;
+
+			return rezultats;
+		}
+
+
+		private double Dalit(double a, double b)
+		{
+			double rezultats = a / b;
+
+			return rezultats;
+		}
+
+
+		private double Kapinat(double a, double b)
+		{
+			double rezultats = a;
+
+			for (int i = 0; i <= a-1; i++)
+			{
+				rezultats = rezultats * a;
+			}
+
+			return rezultats;
+		}
+
+	}
+}
---
+++ Praktiskais-2/Praktiskais-2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktiskais_2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Kalkulators obj = new Kalkulators();
+			obj.funkcizv();
+
+
+			Console.ReadLine();
+
+		}
+	}
+}
